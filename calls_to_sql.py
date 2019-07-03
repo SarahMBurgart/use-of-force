0df12ad811e6c8d7b0b7e6342040c8d07b5c84fc
Sarah Burgart <sarahburgart@Sarahs-MacBook-Air-2.local>
2019-07-03 15:01:48 -0700

    moved code to clean calls csv and move it to sql table to a dot py file

---
+++ calls_to_sql.py
@@ -0,0 +1,45 @@
+# code to clean up calls.csv and move it to a SQL table
+
+import numpy as np
+import pandas as pd
+
+calls = pd.read_csv("data/Call_Data.csv")
+
+def callstime(df, col_arrive):
+    yearmonthday = df[col_arrive].astype(str).str.split(" ")
+    df["year"] = yearmonthday.apply(lambda x: x[2])
+    df["day"] = yearmonthday.apply(lambda x: x[1])
+    month_dict = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec":12}
+    df["month"] = yearmonthday.apply(lambda x: month_dict[x[0]])
+    df["time"] = yearmonthday.apply(lambda x: x[3])
+    return df
+
+calls = callstime(calls,"Arrived Time")
+calls17 = calls[calls["year"].isin(["2014", "2015", "2016", "2017", "2018", "2019"])]
+
+# calculate and add column for epoch time
+bar = pd.to_datetime( calls17["Arrived Time"], format="%b %d %Y %I:%M:%S:%f%p" )
+epoch_second2 = bar.map(lambda x: x.value/1e9)
+calls17["epoch_calls"] = epoch_second2
+
+rs = calls17["CAD Event Number"].value_counts()
+two = []
+for v, i in zip(rs.values, rs.index):
+    if v >1:
+        two.append(i)
+# len(two) == 362  
+st = [x > 1 for x in rs]
+dt =rs[st]
+
+t = calls17[calls17["CAD Event Number"].isin(two)]
+indt = t[t["Event Clearance Description"] == "-"].index
+indt
+
+calls17.drop(indt, axis=0, inplace=True)
+
+#remove repetitive columns
+calls_tosql = calls17.drop(columns =["year","day", "month", "time", "Precinct", "Sector", "Arrived Time"])
+calls_tosql.set_index("epoch_calls", inplace=True)
+
+# move calls to sqlite table - DONT REPEAT
+calls_tosql.to_sql("log", con=cnx, index=True, if_exists="replace")
