cf1f0bfae98f4e49e87798d0c63faa16e78420b6
Sarah Burgart <sarahburgart@Sarahs-MacBook-Air-2.local>
2019-07-06 19:48:45 -0700

    finished init in py file and amended jupyter notebook

--- force.py
+++ force.py
@@ -3,23 +3,23 @@ import numpy as np
 from datetime import datetime
 
 class New_Call:
-    def __init__(gender, race, reason, beat):
-    now = datetime.datetime.now()
-    self.month = now.month
-    self.day = now.day
-    self.hour = now.hour
-    self.day_of_week = now.dt.dayofweek
-    self.beat = beat
-    self.gender = gender
-    self.race = race
-    self.reason = reason
-    self.df = pd.DataFrame(columns=["force_beat", "Subject_Gender", "Subject_Race", "Initial_Call_Type", "month", "day", "day_of_week", "call_hour"])
-    self.df.loc[0] = [self.beat, self.gender, self.race, self.reason, self.month, self.day, self.day_of_week, self.hour]
+    def __init__(self, gender, race, reason, beat):
+        now = datetime.now()
+        self.month = now.month
+        self.day = now.day
+        self.hour = now.hour
+        self.day_of_week = datetime.today().weekday()
+        self.beat = beat
+        self.gender = gender
+        self.race = race
+        self.reason = reason
+        self.df = pd.DataFrame(columns=["force_beat", "Subject_Gender", "Subject_Race", "Initial_Call_Type", "month", "day", "day_of_week", "call_hour"])
+        self.df.loc[0] = [self.beat, self.gender, self.race, self.reason, self.month, self.day, self.day_of_week, self.hour]
 
     # race, reason both need to be drop down menus
 
 
-    def __predict__(self):
+    
 
         self.df = pd.get_dummies(data = self.df, 
                         columns=["force_beat", 
@@ -28,4 +28,4 @@ class New_Call:
                                 "Gender",  "Race", "InitCall"])
     
 
-    return self.df
+        
