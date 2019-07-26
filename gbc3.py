09649c45bd9a6261c2e4dc3c643eeeb5f6db603e
Sarah Burgart <sarahburgart@Sarahs-MacBook-Air-2.local>
2019-07-26 15:18:25 -0700

    two step model predictions

--- gbc3.py
+++ gbc3.py
@@ -9,7 +9,7 @@ def make_model(df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=659)
     
     
-    gbc3 = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=5,
+    gbc3 = GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=1000,
                                 random_state=659, verbose=1)
     
     gbc3.fit(X_train, y_train)
